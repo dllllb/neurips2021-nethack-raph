26267fceb0ea999cb97480664c4a5de33fcdfd43
Eric Hambro <eric.hambro@gmail.com>
2021-06-06 04:54:01 -0700

    Remove unnecessary functions from Batched Agent.

--- agents/batched_agent.py
+++ agents/batched_agent.py
@@ -11,20 +11,9 @@ class BatchedAgent:
         self.num_envs = num_envs
         self.num_actions = num_actions
 
-    def preprocess_observations(self, observations, rewards, dones, infos):
+    def batched_step(self, observations, rewards, dones, infos):
         """
-        Add any preprocessing steps, for example rerodering/stacking for torch/tf in your model
+        Take list of outputs of each environments and return a list of actions
         """
-        pass
+        raise NotImplementedError
 
-    def preprocess_actions(self, actions):
-        """
-        Add any postprocessing steps, for example converting to lists
-        """
-        pass
-
-    def batched_step(self):
-        """
-        Return a list of actions
-        """
-        pass
