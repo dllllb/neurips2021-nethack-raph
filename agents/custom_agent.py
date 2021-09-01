58c31e32f16ab366f0aef65b232ea093a357d7aa
Dmitry Sorokin <dmitrii.sorokin@phystech.edu>
2021-09-01 21:21:45 +0300

    silent

--- agents/custom_agent.py
+++ agents/custom_agent.py
@@ -22,7 +22,7 @@ class CustomAgent(BatchedAgent):
     """A example agent... that simple acts randomly. Adapt to your needs!"""
 
     def init(self):
-        Kernel(silent=False)
+        Kernel(silent=True)
 
         # Stuff
         Console()
